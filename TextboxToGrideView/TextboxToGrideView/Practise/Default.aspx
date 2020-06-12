7c4004101d4c3b85082013204431bc67f34a91ff
pranto.shon <prantoshon14@gmail.com>
2020-06-12 23:44:58 +0600

    done

---
+++ TextboxToGrideView/TextboxToGrideView/Practise/Default.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TextboxToGrideView.Practise.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div> <asp:ScriptManager ID="ScriptManager1" runat="server" />
+
+<asp:UpdatePanel ID="UpdatePanel1" runat="server">
+   <ContentTemplate>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtADDRESS" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtRoll" runat="server"></asp:TextBox>
+        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+      
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None"  BorderWidth="1px" CellPadding="3"  OnDataBound="GridView1_DataBound" OnSelectedIndexChanged="GridView1_SelectedIndexChanged1" OnSelectedIndexChanging="GridView1_SelectedIndexChanging">
+           
+          
+         
+           
+          
+           
+            <Columns>
+                <asp:TemplateField>
+                  
+                    <ItemTemplate>
+                        
+                         <asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="true" OnCheckedChanged="CheckBox1_CheckedChanged" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+           
+          
+         
+           
+          
+           
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+           
+        </asp:GridView>
+          <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Label ID="lblmsg" runat="server" Text="Label"></asp:Label>
+          </ContentTemplate>
+   <Triggers>
+      <asp:AsyncPostbackTrigger ControlID="btnAdd" EventName="Click" />
+   </Triggers>
+</asp:UpdatePanel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TextboxToGrideView/TextboxToGrideView/Practise/Default.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace TextboxToGrideView.Practise
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //dt = new DataTable();
+            //DataColumn dc1 = new DataColumn("NAME");
+            //DataColumn dc2 = new DataColumn("ADDRESS");
+            //dt.Columns.Add(dc1);
+            //dt.Columns.Add(dc2);
+            //DataRow dr1 = dt.NewRow();
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+
+            if (!this.IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[3] {
+            new DataColumn("NAME"), new DataColumn("ADDRESS"),new DataColumn("roll")
+        });
+                ViewState["Emp"] = dt;
+                this.BindGrid();
+
+               
+            }
+
+          
+
+
+
+        }
+        protected void BindGrid()
+        {
+            GridView1.DataSource = (DataTable)ViewState["Emp"];
+            GridView1.DataBind();
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            //DataRow dr1 = dt.NewRow();
+            //dr1[0] = txtName.Text;
+            //dr1[1] = txtADDRESS.Text;
+            //dt.Rows.Add(dr1);
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+
+            DataTable dt = (DataTable)ViewState["Emp"];
+            dt.Rows.Add(txtName.Text.Trim(), txtADDRESS.Text.Trim(), txtRoll.Text.Trim());
+            ViewState["Emp"] = dt;
+            this.BindGrid();
+            txtName.Text = "";
+            txtADDRESS.Text = "";
+            txtRoll.Text = "";
+
+
+          
+
+
+
+
+        }
+
+
+
+
+
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+  
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        protected void GridView1_DataBound(object sender, EventArgs e)
+        {
+           
+        }
+        string str = string.Empty;
+        string strname = string.Empty;
+        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+
+            
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string empidstr = string.Empty;
+
+            string empnamestr = string.Empty;
+
+            foreach (GridViewRow gridrows in GridView1.Rows)
+
+            {
+
+                CheckBox chkbox = (CheckBox)gridrows.FindControl("CheckBox1");
+
+                if (chkbox != null & chkbox.Checked)
+
+                {
+
+                 //   empidstr += gvDetails.DataKeys[gridrows.RowIndex].Value.ToString() + ',';
+
+                    empnamestr += gridrows.Cells[3].Text + ',';
+
+                }
+
+            }
+
+            empidstr = empidstr.Trim(",".ToCharArray());
+
+            empnamestr = empnamestr.Trim(",".ToCharArray());
+
+            lblmsg.Text =  "</b><br/>" + "Employee Name: <b>" + empnamestr + "</b>";
+        }
+    }
+}
